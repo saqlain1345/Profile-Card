f21566fd8f64270e972b5a4c6642224ca6727c5e
saqlain1345 <saqlainkhan1345@gmail.com>
2021-03-12 21:46:43 +0530

    second commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// var i = 0;
+// var text = "Hello It's Saqlain Khan";
+// function typing() {
+//     if (i < text.length) {
+//         document.getElementById("text").innerHTML += text.charAt(i);
+//         i++;
+//         setTimeout(typing, 150);
+//         if (i == 6) {
+//             document.getElementById("text").innerHTML += "</br>";
+//         }
+//      }
+// }
+// typing();
